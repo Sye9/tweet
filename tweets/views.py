da9735172a7f75e613a4b8dd24af1664e3d1affb
dagusen <louisaleksieb.dagusen@gmail.com>
2018-01-27 20:20:47 +0800

    added deleteview

--- tweets/views.py
+++ tweets/views.py
@@ -5,12 +5,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.shortcuts import render, get_object_or_404
 
+from django.urls import reverse_lazy
+
 from django.views.generic import (
-	DetailView,
-	ListView,
 	CreateView,
-	UpdateView,
-	DeleteView
+	DetailView,
+	DeleteView,
+	ListView,
+	UpdateView
 	)
 
 from .forms import TweetModelForm
@@ -27,27 +29,24 @@ class TweetCreateView(LoginRequiredMixin, FormUserNeededMixin, CreateView):
 	success_url = "/tweet/create/"
 	login_url = "/admin/login/"
 
-
 class TweetUpdateView(UserOwnerMixin, LoginRequiredMixin, UpdateView):
 	queryset = Tweet.objects.all()
 	form_class = TweetModelForm
 	template_name = 'tweets/update_view.html'
 	success_url = '/tweet/'
 
+class TweetDeleteView(LoginRequiredMixin, DeleteView):
+	model = Tweet
+	success_url = reverse_lazy("home")
+	template_name = 'tweets/delete_confirm.html'
+	
+
 class TweetDetailView(DetailView):
 	queryset = Tweet.objects.all()
-
-	# def get_object(self):
-	# 	print(self.kwargs)
-	# 	pk = self.kwargs.get("pk")
-	# 	obj = get_object_or_404(Tweet, pk=pk)
-	# 	return obj
 
 class TweetListView(ListView):
 	queryset = Tweet.objects.all()
 
 	def get_context_data(self, *args, **kwargs):
 		context = super(TweetListView, self).get_context_data(*args, **kwargs)
-		# context["another_list"] = Tweet.objects.all()
-		# print(context)
 		return context
