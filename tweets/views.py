cc1af40ebe825e23ea09d7675f66f94100b74461
dagusen <louisaleksieb.dagusen@gmail.com>
2018-01-27 17:24:47 +0800

    modified view class base views

--- tweets/views.py
+++ tweets/views.py
@@ -10,30 +10,19 @@ from .models import Tweet
 # Create your views here.
 
 class TweetDetailView(DetailView):
-	template_name = "tweets/detail_view.html"
 	queryset = Tweet.objects.all()
 
 	def get_object(self):
-		return Tweet.object.get(id=1)
+		print(self.kwargs)
+		pk = self.kwargs.get("pk")
+		print(pk)
+		return Tweet.object.get(id=pk)
 
 class TweetListView(ListView):
-	template_name = "tweets/list_view.html"
 	queryset = Tweet.objects.all()
 
-# def tweet_detail_view(request, id=1):
-# 	queryset = Tweet.objects.get(id=id)
-# 	print(queryset)
-# 	context = {
-# 		'object': queryset
-# 	}
-# 	return render(request, "tweets/detail_view.html", context)
-
-# def tweet_list_view(request):
-# 	queryset = Tweet.objects.all()
-# 	print(queryset)
-# 	for obj in queryset:
-# 		print(obj)
-# 	context = {
-# 		'object_list': queryset
-# 	}
-# 	return render(request, "tweets/list_view.html", context)
+	def get_context_data(self, *args, **kwargs):
+		context = super(TweetListView, self).get_context_data(*args, **kwargs)
+		# context["another_list"] = Tweet.objects.all()
+		# print(context)
+		return context
