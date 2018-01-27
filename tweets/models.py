4b3f74a8b427a999a9d44c9fbf1d2681b542a1e4
dagusen <louisaleksieb.dagusen@gmail.com>
2018-01-27 18:27:15 +0800

    added validators in models content

--- tweets/models.py
+++ tweets/models.py
@@ -3,23 +3,31 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 
+from django.core.exceptions import ValidationError
+
 from django.db import models
 
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
 
+def validate_content(value):
+	content = value
+	if content == "abc":
+		raise ValidationError("Content cannot be ABC")
+	return value
+
 class Tweet(models.Model):
 	user 			= models.ForeignKey(User)
-	content 		= models.CharField(max_length=140)
+	content 		= models.CharField(max_length=140, validators=[validate_content])
 	updated 		= models.DateTimeField(auto_now=True)
 	timestamp		= models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.content
 
-	def clean(self, *args, **kwargs):
-		content = self.content
-		if content == "abc":
-			raise ValidationError("Cannot be ABC")
-		return super(Tweet, self).clean(*args, **kwargs)
+	# def clean(self, *args, **kwargs):
+	# 	content = self.content
+	# 	if content == "abc":
+	# 		raise ValidationError("Content cannot be ABC")
+	# 	return super(Tweet, self).clean(*args, **kwargs)
