053b36baabd83794ca0c749f03cde88a87a6ff25
dagusen <louisaleksieb.dagusen@gmail.com>
2018-01-27 12:46:19 +0800

    added user in Tweet app

--- tweets/models.py
+++ tweets/models.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.conf import settings
+
 from django.db import models
 
 # Create your models here.
 
+User = settings.AUTH_USER_MODEL
+
 class Tweet(models.Model):
+	user 			= models.ForeignKey(User)
 	content 		= models.CharField(max_length=140)
 	updated 		= models.DateTimeField(auto_now=True)
 	timestamp		= models.DateTimeField(auto_now_add=True)
