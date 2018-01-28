c10e3416ca7b4db1b07e7c24465b164b3a051014
dagusen <louisaleksieb.dagusen@gmail.com>
2018-01-28 19:30:50 +0800

    API serializer view

--- project/settings/local.py
+++ project/settings/local.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms',
+    'rest_framework',
+
     'tweets'
 ]
 
@@ -126,3 +129,5 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-serve")
+
+# CRISPY_TEMPLATE_PACK = 'boostrap3'
