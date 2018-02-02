136e15133a5452834d56c25d91ddf7979fc9f59a
dagusen <louisaleksieb.dagusen@gmail.com>
2018-02-02 16:19:19 +0800

    jQuery create

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
 from .views import (
-		TweetListAPIView
+		TweetListAPIView,
+		TweetCreateAPIView
 	)
 
 urlpatterns = [
 	url(r'^$', TweetListAPIView.as_view(), name='list'),
+	url(r'^create/$', TweetCreateAPIView.as_view(), name='create')
 ]
