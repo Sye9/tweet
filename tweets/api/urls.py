3ad9238f334683cc7fdb2fe749d9c9b5e369a199
dagusen <louisaleksieb.dagusen@gmail.com>
2018-02-03 21:16:11 +0800

    installed pytz

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 
 from .views import (
+		RetweetAPIView,
 		TweetListAPIView,
 		TweetCreateAPIView
 	)
 
 urlpatterns = [
 	url(r'^$', TweetListAPIView.as_view(), name='list'),
-	url(r'^create/$', TweetCreateAPIView.as_view(), name='create')
+	url(r'^create/$', TweetCreateAPIView.as_view(), name='create'),
+	url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet'),
 ]
