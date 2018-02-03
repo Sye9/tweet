3aa619bdf8033ec6f16f4c30ef587121cd68564b
dagusen <louisaleksieb.dagusen@gmail.com>
2018-02-03 12:25:24 +0800

    User profile followers

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
+
 from django.db import models
 
+User = settings.AUTH_USER_MODEL
+
 # Create your models here.
+
+
+class UserProfile(models.Model):
+	user 		= models.OneToOneField(User, related_name='profile')
+	following 	= models.ManyToManyField(User, blank=True, related_name='followed_by')
+
+	def __str__(self):
+		return str(self.following.all().count())
