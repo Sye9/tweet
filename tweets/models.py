9dc67c3e383ab9844c0063afee144242f1117f41
dagusen <louisaleksieb.dagusen@gmail.com>
2018-02-04 10:16:20 +0800

    Parse Username Hastag with regex

--- tweets/models.py
+++ tweets/models.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import re
+
 from django.conf import settings
 
 from django.urls import reverse
 
 from django.db import models
+
+from django.db.models.signals import post_save
 
 from django.utils import timezone
 
@@ -56,3 +60,15 @@ class Tweet(models.Model):
 
 	class Meta:
 		ordering = ['-updated', '-timestamp']
+
+def tweet_save_receiver(sender, instance, created, *args, **kwargs):
+	if created and not instance.parent:
+		user_regex = r'@(?P<username>[\w.@+-]+)'
+		usernames = re.findall(user_regex, instance.content)
+		
+
+		hash_regex = r'#(?P<hashtag>[\w\d-]+)'
+		hashtags = re.findall(hash_regex, instance.content)
+		
+
+post_save.connect(tweet_save_receiver, sender=Tweet)
